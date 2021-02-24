e0d44018414ccad966a04cff16ae0a82884c5220
Todd Haskell <todd@craggypeak.com>
2021-02-23 18:37:26 -0800

    Tidying up main branch

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="salal",
-    version="0.10.0-alpha",
+    version="0.10.1-alpha",
     author="Todd Haskell",
     author_email="todd@craggypeak.com",
     description="A system for building websites from templates and content",
@@ -21,10 +21,11 @@ setuptools.setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
         "Intended Audience :: Developers",
-        "Topic::Text Processing::Markup::HTML"
+        "Topic :: Text Processing :: Markup :: HTML"
     ],
     python_requires='>=3.6',
     install_requires=[
-        'Jinja2>=2.11.2'
+        'Jinja2>=2.11.2',
+        'lxml>=4.1.0'
     ]
 )
